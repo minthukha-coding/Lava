31c74720676013f2feb6b55ff02af5d93d1e0ef6
minthukha1288 <minthukha1239788@gmail.com>
2023-10-06 11:10:04 +0630

    0.3

--- src/pages/sabscribe/index.jsx
+++ src/pages/sabscribe/index.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Sabscribe() {
   return (
     <>
-      <div className="text-center bg-black text-white p-10">
+      <div className="text-center bg-slate-800 text-white p-10">
         <h1 className="text-5xl font-bold">Sabscribe</h1>
         <h5 className="pb-5 pt-5 -tracking-tight">SING-UP TO OUR NEWLETTERS</h5>
         <input
           type="text"
           placeholder="Your email"
-          className="bg-slate-800 w-[400px] mb-5 p-[10px] rounded-md"
+          className="bg-slate-800 border-2 border-black w-[400px] mb-5 p-[10px] rounded-md"
         />
         <br />
         <button className="px-10 py-2 border-2 rounded-sm ">SABMIT</button>
